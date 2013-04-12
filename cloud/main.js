8413e6a97bad901fbd1bca3835ba12823a6c48a4
dennis zhuang <killme2008@gmail.com>
2013-04-12 16:38:06 +0800

    Added beforeSave/afterSave functions

--- cloud/main.js
+++ cloud/main.js
@@ -6,3 +6,31 @@ AV.Cloud.define("hello", function(request, response) {
 	console.log(name.isACoolName('Skippy')); // returns true;
 	response.success("Hello world," + request.params.name);
 });
+
+AV.Cloud.beforeSave("TestReview", function(req, res){
+	if (request.object.get("stars") < 1) {
+		response.error("you cannot give less than one star");
+	} else if (request.object.get("stars") > 5) {
+		response.error("you cannot give more than five stars");
+	} else {
+		var comment = request.object.get("comment");
+		if (comment && comment.length > 140) {
+			// Truncate and add a ...
+			request.object.set("comment", comment.substring(0, 137) + "...");
+		}
+		response.success();
+	}
+});
+
+AV.Cloud.afterSave("TestReview", function(request) {
+	var query = new AV.Query("TestPost");
+	query.get(request.object.get("post").id, {
+		success: function(post) {
+			post.increment("comments");
+			post.save();
+		},
+		error: function(error) {
+			throw "Got an error " + error.code + " : " + error.message;
+		}
+	});
+});
