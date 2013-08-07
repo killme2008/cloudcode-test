ae177b86acb976be7a4cdf4cc7c8ae67db87a939
dennis zhuang <killme2008@gmail.com>
2013-08-07 11:45:43 +0800

    Added app.js

---
+++ cloud/app.js
@@ -0,0 +1,17 @@
+// These two lines are required to initialize Express in Cloud Code.
+var express = require('express');
+var app = express();
+
+// Global app configuration section
+app.set('views', 'cloud/views');  // Specify the folder to find templates
+app.set('view engine', 'ejs');    // Set the template engine
+app.use(express.bodyParser());    // Middleware for reading request body
+
+// This is an example of hooking up a request handler with a specific request
+// path and HTTP verb using the Express routing API.
+app.get('/hello', function(req, res) {
+  res.render('hello', { message: 'Congrats, you just set up your app!' });
+});
+
+// This line is required to make Express respond to http requests.
+app.listen();
