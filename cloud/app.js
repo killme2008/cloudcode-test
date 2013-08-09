6619b57b255aac6d70ecdba3fec6862fc8eed220
dennis zhuang <killme2008@gmail.com>
2013-08-09 12:07:25 +0800

    Added visitors

--- cloud/app.js
+++ cloud/app.js
@@ -10,15 +10,47 @@ app.use(express.bodyParser());    // Middleware for reading request body
 // This is an example of hooking up a request handler with a specific request
 // path and HTTP verb using the Express routing API.
 app.get('/hello', function(req, res) {
-  res.render('hello', { message: 'Congrats, you just set up your app!' });
+	res.render('hello', { message: 'Congrats, you just set up your app!' });
 });
 app.enable('trust proxy');
+
 app.get('/', function(req, res){
 	res.render('index',{ name: "AVOSCloud"});
 });
+
+var Visitor = AV.Object.extend('Visitor');
+function renderIndex(res, name){
+	var query = new AV.Query(Visitor);
+	query.skip(0);
+	query.limit(10);
+	query.find({
+		success: function(results){
+			res.render('index',{ name: name, visitors: results});
+		},
+		error: function(error){
+			res.render('500',500)
+		}
+	});
+
+}
+
 app.post('/',function(req, res){
 	var name = req.body.name;
-	res.render('index',{ name: name});
+	if(name && name!=''){
+		//Save visitor
+		var visitor = new Visitor();
+		visitor.set('name', name);
+		visitor.save(null, {
+			success: function(gameScore) {
+				renderIndex(res, name);
+			},
+			error: function(gameScore, error) {
+				res.render('500', 500);
+			}
+		});
+	}else{
+		renderIndex(res, 'AVOSCloud');
+	}
 });
 
 // This line is required to make Express respond to http requests.
