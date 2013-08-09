33ba0b046a0e8412ebe86f5a23dfafe2c85fd198
dennis zhuang <killme2008@gmail.com>
2013-08-09 12:53:56 +0800

    Remove Mr.

--- cloud/app.js
+++ cloud/app.js
@@ -22,7 +22,6 @@ function renderIndex(res, name){
 	query.descending('createdAt');
 	query.find({
 		success: function(results){
-			name = 'Mr. ' + name;
 			res.render('index',{ name: name, visitors: results});
 		},
 		error: function(error){
