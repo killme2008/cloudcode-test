33d24115f58081fab001ff91804b13e2a60f3a5f
dennis zhuang <killme2008@gmail.com>
2013-08-13 15:21:53 +0800

    Update app id and key

--- cloud/app.js
+++ cloud/app.js
@@ -1,18 +1,21 @@
-// These two lines are required to initialize Express in Cloud Code.
+// 在Cloud code里初始化express框架
 var express = require('express');
 var app = express();
+var name = require('cloud/name.js');
 
-// Global app configuration section
-app.set('views', 'cloud/views');  // Specify the folder to find templates
-app.set('view engine', 'ejs');    // Set the template engine
-app.use(express.bodyParser());    // Middleware for reading request body
+// App全局配置
+//设置模板目录
+if(__production)
+	app.set('views', 'cloud/views');
+else
+	app.set('views', 'cloud/dev_views');
+app.set('view engine', 'ejs');    // 设置template引擎
+app.use(express.bodyParser());    // 读取请求body的中间件
 
-// This is an example of hooking up a request handler with a specific request
-// path and HTTP verb using the Express routing API.
+//使用express路由API服务/hello的http GET请求
 app.get('/hello', function(req, res) {
 	res.render('hello', { message: 'Congrats, you just set up your app!' });
 });
-app.enable('trust proxy');
 
 var Visitor = AV.Object.extend('Visitor');
 function renderIndex(res, name){
